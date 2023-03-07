aed50daaa5279949f3da9b4fbd2d08366e610f54
aswinr <aswinr019@gmail.com>
2023-03-07 22:28:24 +0530

    Added db connection

--- app.js
+++ app.js
@@ -7,9 +7,22 @@ const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT,() => {
-  console.log(`listening on http://localhost:${PORT}`);
-});
+const dbURI = `mongodb+srv://${process.env.DBUser}:${process.env.DBPass}@cluster0.430qrv7.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(dbURI)
+  .then((result) => {
+  app.listen(PORT,() => {
+    console.log(`listening on http://localhost:${PORT}`);
+  });
+})
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+//app.listen(PORT,() => {
+//  console.log(`listening on http://localhost:${PORT}`);
+//});
 
 
 app.get("/",(req,res) => {
