c8f056b9796dd92fd0ae6c5568bbb0bb725e26de
aswinr <aswinr019@gmail.com>
2023-03-07 22:28:08 +0530

    Added schema for destinations

--- models/destinations.js
+++ models/destinations.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const destinationSchema = new Schema({});
+const destinationSchema = new Schema({
+  name: String,
+  location: {
+    type: String,
+    coordinates: [Number]
+  }
+},{ timestamps: true });
 
 const Destination = mongoose.model("Destination",desinationSchema);
 
