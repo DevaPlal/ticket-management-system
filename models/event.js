041a025c3b1a13c07c164fe752627860a45d4146
aswinr <aswinr019@gmail.com>
2023-03-13 22:55:13 +0530

    added event schema

--- models/event.js
+++ models/event.js
@@ -3,7 +3,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-  
+  name:{
+    type: String,
+    required: true,
+    unique: [true,"Event already registered"],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  venue: {
+    type: String,
+    required: true
+  },
+  totalSeats: {
+    type: Number,
+    required: true
+  },
+  availableSeats: {
+    type: Number,
+    required: true
+  }
 },
   { timestamps: true });
 
