93aaec453eef36d264349e34730215d9a0d66f83
aswinr <aswinr019@gmail.com>
2023-03-13 14:20:48 +0530

    Added CORS headers

--- app.js
+++ app.js
@@ -26,6 +26,15 @@ mongoose.connect(process.env.DBURI,{
     console.log(err);
   });
 
+//set up CORS headers
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
