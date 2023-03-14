55eee0bfacd247c0fe348e1bfbfd1b8cf86d5d76
aswinr <aswinr019@gmail.com>
2023-03-14 07:40:11 +0530

    updates in event model

--- models/event.js
+++ models/event.js
@@ -10,15 +10,23 @@ const eventSchema = new Schema({
   },
   description: {
     type: String,
-    required: true,
+    required: [true, "Please provide a description"]
   },
   venue: {
+    type: String,
+    required: [true, "Please specify a venue"],
+  },
+  date: {
+    type: date,
+    required: [true, "Please specify a date for the event"]
+  },
+  status: {
     type: String,
     required: true
   },
   totalSeats: {
     type: Number,
-    required: true
+    required: [true, "Please provide the number of seats"]
   },
   availableSeats: {
     type: Number,
